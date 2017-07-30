0040ffec765c2359ea40e9ca891d69883579bf66
djx <djx@localhost.localdomain>
2017-07-30 16:30:05 +0800

    e

--- test17_confirm.py
+++ test17_confirm.py
@@ -14,24 +14,28 @@ for i in range(nrows):
     data_rows.append(tab.row_values(i))
 
 for row in data_rows:
-    datas = [row[0],row[1],row[2],'2017',row[3],'234300']
-    br.get("http://60.173.222.146/firstWeb/szksLogin.jsp?loginType=01")
-    time.sleep(1)
-    br.find_element_by_id('grq_szksdl_fxm').send_keys(row[0])
-    time.sleep(0.5)
-    br.find_element_by_id('grq_szksdl_fsfzh').send_keys(row[1])
-    time.sleep(0.5)
-    br.find_element_by_id('grq_szksdl_fdhhm').send_keys(row[2])
-    time.sleep(1)
+    try:
+        print(row[0])
+        datas = [row[0],row[1],row[2],'2017',row[3],'234300']
+        br.get("http://60.173.222.146/firstWeb/szksLogin.jsp?loginType=01")
+        time.sleep(1)
+        br.find_element_by_id('grq_szksdl_fxm').send_keys(row[0])
+        time.sleep(0.5)
+        br.find_element_by_id('grq_szksdl_fsfzh').send_keys(row[1])
+        time.sleep(0.5)
+        br.find_element_by_id('grq_szksdl_fdhhm').send_keys(row[2])
+        time.sleep(1)
 
-    vtxt = input('captcha:')
-    br.find_element_by_id('jcaptcha').send_keys(vtxt)
+        vtxt = input('captcha:')
+        br.find_element_by_id('jcaptcha').send_keys(vtxt)
 
-    br.find_element_by_id('login-btn').click()
-    time.sleep(2)
-    br.find_element_by_partial_link_text('报考中职学校').click()
-    
-    c = input(':')
-    if c == 'q':
-        break
+        br.find_element_by_id('login-btn').click()
+        time.sleep(2)
+        br.find_element_by_partial_link_text('报考中职学校').click()
+        
+        c = input(':')
+        if c == 'q':
+            break
+    except:
+        print('error')
 br.quit()
